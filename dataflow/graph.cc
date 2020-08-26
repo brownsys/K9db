aec805f375df86ae9d9a3567f48e583859065b4b
Malte Schwarzkopf <malte@cs.brown.edu>
2020-08-25 22:46:26 -0400

    clang-format and warning

--- dataflow/graph.cc
+++ dataflow/graph.cc
@@ -1,5 +1,5 @@
-#include "dataflow/edge.h"
 #include "dataflow/graph.h"
+#include "dataflow/edge.h"
 
 #include <memory>
 
@@ -15,7 +15,8 @@ bool DataFlowGraph::AddNode(OperatorType type, std::shared_ptr<Operator> op) {
   return res.second;
 }
 
-bool DataFlowGraph::AddEdge(std::shared_ptr<Operator> op1, std::shared_ptr<Operator> op2) {
+bool DataFlowGraph::AddEdge(std::shared_ptr<Operator> op1,
+                            std::shared_ptr<Operator> op2) {
   EdgeIndex idx = mint_edge_index();
   Edge edge(op1, op2);
 
@@ -26,6 +27,8 @@ bool DataFlowGraph::AddEdge(std::shared_ptr<Operator> op1, std::shared_ptr<Opera
 
 std::vector<std::shared_ptr<Operator>> inputs() {
   // TODO get all input operators
+  std::vector<std::shared_ptr<Operator>> v;
+  return v;
 }
 
 }  // namespace dataflow
