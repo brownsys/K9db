bd949c8a8958a4b8ef95ecdee1e26a5201501a44
Kinan Dak Albab <kinan.bab.14@gmail.com>
2021-02-28 02:59:16 -0500

    clean up old files

--- pelton/planner/calcite/src/com/brownsys/pelton/.old/operators/MatviewOperator.java
+++
@@ -1,9 +0,0 @@
-package com.brownsys.pelton.planner.operators;
-
-import com.brownsys.pelton.planner.graph.Operator;
-
-public class MatviewOperator extends Operator {
-  public MatviewOperator() {
-    this.name = "Matview";
-  }
-}
