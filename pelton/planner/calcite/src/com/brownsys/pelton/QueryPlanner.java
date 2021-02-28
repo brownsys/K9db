ad786682b1398c69574e9883c5ab9bc50212dde1
Kinan Dak Albab <kinan.bab.14@gmail.com>
2021-02-28 15:13:19 -0500

    clean up

--- pelton/planner/calcite/src/com/brownsys/pelton/QueryPlanner.java
+++ pelton/planner/calcite/src/com/brownsys/pelton/QueryPlanner.java
@@ -2,16 +2,9 @@ package com.brownsys.pelton;
 
 import com.brownsys.pelton.nativelib.DataFlowGraphLibrary;
 import com.brownsys.pelton.schema.PeltonSchemaFactory;
-import java.util.ArrayList;
 import org.apache.calcite.config.Lex;
-import org.apache.calcite.interpreter.Bindables;
 import org.apache.calcite.plan.Contexts;
-import org.apache.calcite.plan.ConventionTraitDef;
-import org.apache.calcite.plan.RelTraitDef;
-import org.apache.calcite.rel.RelCollationTraitDef;
 import org.apache.calcite.rel.RelNode;
-import org.apache.calcite.rel.rules.AggregateProjectMergeRule;
-import org.apache.calcite.rel.type.RelDataTypeSystem;
 import org.apache.calcite.schema.SchemaPlus;
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.parser.SqlParseException;
@@ -20,8 +13,6 @@ import org.apache.calcite.tools.FrameworkConfig;
 import org.apache.calcite.tools.Frameworks;
 import org.apache.calcite.tools.Planner;
 import org.apache.calcite.tools.RelConversionException;
-import org.apache.calcite.tools.RuleSet;
-import org.apache.calcite.tools.RuleSets;
 import org.apache.calcite.tools.ValidationException;
 
 public class QueryPlanner {
@@ -40,36 +31,15 @@ public class QueryPlanner {
     PeltonSchemaFactory schemaFactory = new PeltonSchemaFactory(generator);
     this.schema = schemaFactory.createSchema();
 
-    ArrayList<RelTraitDef> traitDefs = new ArrayList<RelTraitDef>();
-    traitDefs.add(ConventionTraitDef.INSTANCE);
-    traitDefs.add(RelCollationTraitDef.INSTANCE);
-
-    final RuleSet customRuleSet =
-        RuleSets.ofList(
-            AggregateProjectMergeRule.INSTANCE,
-            Bindables.BINDABLE_VALUES_RULE,
-            Bindables.BINDABLE_TABLE_SCAN_RULE,
-            Bindables.BINDABLE_PROJECT_RULE,
-            Bindables.BINDABLE_FILTER_RULE,
-            Bindables.BINDABLE_AGGREGATE_RULE);
-    // Bindables.BINDABLE_UNION_RULE);
-
     FrameworkConfig calciteFrameworkConfig =
         Frameworks.newConfigBuilder()
             // MySQL dialect.
             .parserConfig(SqlParser.configBuilder().setLex(Lex.MYSQL).build())
             // Sets the schema to use by the planner
             .defaultSchema(this.schema)
-            // .traitDefs(traitDefs)
             // Context provides a way to store data within the planner session that can be
             // accessed in planner rules.
             .context(Contexts.EMPTY_CONTEXT)
-            // Rule sets to use in transformation phases. Each transformation phase can use
-            // a different set of rules.
-            // .ruleSets(customRuleSet)
-            // Custom cost factory to use during optimization
-            // .costFactory(null)
-            .typeSystem(RelDataTypeSystem.DEFAULT)
             .build();
 
     this.planner = Frameworks.getPlanner(calciteFrameworkConfig);
@@ -80,15 +50,13 @@ public class QueryPlanner {
     SqlNode sqlNode;
 
     try {
-      sqlNode = planner.parse(query);
+      sqlNode = this.planner.parse(query);
     } catch (SqlParseException e) {
       throw new RuntimeException("Query parsing error.", e);
     }
 
-    SqlNode validatedSqlNode = planner.validate(sqlNode);
-    return planner.rel(validatedSqlNode).project();
-    // return planner.transform(0, logicalPlan.getTraitSet().plus(BindableConvention.INSTANCE),
-    // logicalPlan);
+    SqlNode validatedSqlNode = this.planner.validate(sqlNode);
+    return this.planner.rel(validatedSqlNode).project();
   }
 
   // Traverse the logical plan and use it to populate the corresponding operators into the graph in
