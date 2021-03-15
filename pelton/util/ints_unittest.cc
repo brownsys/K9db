c98976ae49c311a82527926bd9f8d4f29d997bbe
Kinan Dak Albab <kinan.bab.14@gmail.com>
2021-03-15 15:34:11 -0400

    Improve CI and code compilation portability
    * Add MacOS ci as well as ubuntu
    * Clean up ci scripts
    * Implement a portable way of specifying whether a literal is uint64_t or int64_t with its unit tests
    * Code formatting / linting
    * Downgrade jni version to 1.8 so that it is compatible with mac default jdk (java 8)

---
+++ pelton/util/ints_unittest.cc
@@ -0,0 +1,40 @@
+#include "pelton/util/ints.h"
+
+#include <string>
+
+#include "gtest/gtest.h"
+
+namespace pelton {
+
+// Test key size.
+TEST(IntsHelpers, SignedTest) {
+  int64_t x0 = 0;
+  int64_t x1 = 1;
+  int64_t x2 = -1;
+  int64_t x3 = 9223372036854775807ull;
+  int64_t x4 = -9223372036854775808ull;
+  EXPECT_EQ(x0, 0_s);
+  EXPECT_EQ(x1, 1_s);
+  EXPECT_EQ(x2, -1_s);
+  EXPECT_EQ(x3, 9223372036854775807_s);
+  EXPECT_EQ(x4, -9223372036854775808_s);
+  EXPECT_EQ("0", std::to_string(0_s));
+  EXPECT_EQ("1", std::to_string(1_s));
+  EXPECT_EQ("-1", std::to_string(-1_s));
+  EXPECT_EQ("9223372036854775807", std::to_string(9223372036854775807_s));
+  EXPECT_EQ("-9223372036854775808", std::to_string(-9223372036854775808_s));
+}
+
+TEST(IntsHelpers, UnsignedTest) {
+  uint64_t x0 = 0;
+  uint64_t x1 = 1;
+  uint64_t x2 = 18446744073709551615ull;
+  EXPECT_EQ(x0, 0_u);
+  EXPECT_EQ(x1, 1_u);
+  EXPECT_EQ(x2, 18446744073709551615_u);
+  EXPECT_EQ("0", std::to_string(0_u));
+  EXPECT_EQ("1", std::to_string(1_u));
+  EXPECT_EQ("18446744073709551615", std::to_string(18446744073709551615_u));
+}
+
+}  // namespace pelton
