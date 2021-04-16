3559613bfcf41c7f696b35e89f19c043d4837c64
Kinan Dak Albab <kinan.bab.14@gmail.com>
2021-04-16 14:14:21 -0400

    Hashing solution to simplify shard db names

--- pelton/shards/sqlengine/util.cc
+++ pelton/shards/sqlengine/util.cc
@@ -2,3 +2,26 @@
 // name (suffixes etc) of things.
 
 #include "pelton/shards/sqlengine/util.h"
+
+#include <openssl/md5.h>
+
+#include <cstdio>
+
+namespace pelton {
+namespace shards {
+namespace sqlengine {
+
+std::string NameShard(const ShardKind &shard_kind, const UserId &user_id) {
+  char hex_buffer[MD5_DIGEST_LENGTH * 2 + 1];
+  unsigned char md5_buffer[MD5_DIGEST_LENGTH];
+  MD5(reinterpret_cast<const unsigned char *>(user_id.c_str()), user_id.size(),
+      md5_buffer);
+  for (size_t i = 0; i < MD5_DIGEST_LENGTH; i++) {
+    snprintf(hex_buffer + (i * 2), 3, "%02x", md5_buffer[i]);
+  }
+  return absl::StrCat(shard_kind, "_", hex_buffer);
+}
+
+}  // namespace sqlengine
+}  // namespace shards
+}  // namespace pelton
