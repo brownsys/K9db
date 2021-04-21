aeab9790ffe278bbdb4ffbda444c5e8cc97500c7
Malte Schwarzkopf <malte@cs.brown.edu>
2021-04-21 12:28:32 -0400

    Add hat-related tables to Lobsters schema

--- experiments/lobsters/schema-simplified.sql
+++ experiments/lobsters/schema-simplified.sql
@@ -23,3 +23,26 @@ CREATE TABLE `comments` ( \
   `hat_id` int, \
   FOREIGN KEY (`OWNER_user_id`) REFERENCES users(id) \
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `hat_requests` ( \
+	`id` int NOT NULL PRIMARY KEY, \
+	`created_at` datetime, \
+	`updated_at` datetime, \
+	`OWNER_user_id` int, \
+	`hat` varchar(255), \
+	`link` varchar(255), \
+	`comment` text, \
+  FOREIGN KEY (`OWNER_user_id`) REFERENCES users(id) \
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `hats` ( \
+	`id` int NOT NULL PRIMARY KEY, \
+	`created_at` datetime, \
+	`updated_at` datetime, \
+	`OWNER_user_id` int, \
+	`OWNER_granted_by_user_id` int, \
+	`hat` varchar(255) NOT NULL, \
+	`link` varchar(255), \
+	`modlog_use` int, \
+	`doffed_at` datetime, \
+  FOREIGN KEY (`OWNER_user_id`) REFERENCES users(id), \
+  FOREIGN KEY (`OWNER_granted_by_user_id`) REFERENCES users(id) \
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
