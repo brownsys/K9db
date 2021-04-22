e8609100930eece694e510e938bf497df0bf4df6
Malte Schwarzkopf <malte@cs.brown.edu>
2021-04-21 22:11:30 -0400

    All Lobsters tables in simplified schema
    Seems to work up to the point of the first insert.

--- experiments/lobsters/schema-simplified.sql
+++ experiments/lobsters/schema-simplified.sql
@@ -74,9 +74,8 @@ CREATE TABLE invitations ( \
   FOREIGN KEY (OWNER_user_id) REFERENCES users(id) \
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 CREATE TABLE keystores ( \
-	`key` varchar(50) NOT NULL, \
-	value int, \
-	PRIMARY KEY (`key`) \
+	`key` varchar(50) NOT NULL PRIMARY KEY, \
+	value int \
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE messages ( \
 	id int NOT NULL PRIMARY KEY, \
@@ -105,7 +104,89 @@ CREATE TABLE moderations ( \
 	is_from_suggestions int, \
   FOREIGN KEY (OWNER_moderator_user_id) REFERENCES users(id) \
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+CREATE TABLE read_ribbons ( \
+	id int NOT NULL PRIMARY KEY, \
+	is_following int, \
+	created_at datetime NOT NULL, \
+	updated_at datetime NOT NULL, \
+	OWNER_user_id int, \
+	story_id int, \
+  FOREIGN KEY (OWNER_user_id) REFERENCES users(id) \
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE saved_stories ( \
+	id int NOT NULL PRIMARY KEY, \
+	created_at datetime NOT NULL, \
+	updated_at datetime NOT NULL, \
+	OWNER_user_id int, \
+	story_id int, \
+  FOREIGN KEY (OWNER_user_id) REFERENCES users(id) \
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE stories ( \
+	id int NOT NULL PRIMARY KEY, \
+	created_at datetime, \
+	OWNER_user_id int, \
+	url varchar(250), \
+	title varchar(150) NOT NULL, \
+	description text, \
+	short_id varchar(6) NOT NULL, \
+	is_expired int NOT NULL, \
+	upvotes int NOT NULL, \
+	downvotes int NOT NULL, \
+	is_moderated int NOT NULL, \
+	hotness int NOT NULL, \
+	markeddown_description text, \
+	story_cache text, \
+	comments_count int NOT NULL, \
+	merged_story_id int, \
+	unavailable_at datetime, \
+	twitter_id varchar(20), \
+	user_is_author int, \
+  FOREIGN KEY (OWNER_user_id) REFERENCES users(id) \
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE suggested_taggings ( \
+	id int NOT NULL PRIMARY KEY, \
+	story_id int, \
+	tag_id int, \
+	OWNER_user_id int, \
+  FOREIGN KEY (OWNER_user_id) REFERENCES users(id) \
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE suggested_titles ( \
+	id int NOT NULL PRIMARY KEY, \
+	story_id int, \
+	OWNER_user_id int, \
+	title varchar(150) NOT NULL, \
+  FOREIGN KEY (OWNER_user_id) REFERENCES users(id) \
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE tag_filters ( \
+	id int NOT NULL PRIMARY KEY, \
+	created_at datetime NOT NULL, \
+	updated_at datetime NOT NULL, \
+	OWNER_user_id int, \
+	tag_id int, \
+  FOREIGN KEY (OWNER_user_id) REFERENCES users(id) \
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE taggings ( \
+	id int NOT NULL PRIMARY KEY, \
+	story_id int NOT NULL, \
+	tag_id int NOT NULL \
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE tags ( \
+	id int NOT NULL PRIMARY KEY, \
+	tag varchar(25) NOT NULL, \
+	description varchar(100), \
+	privileged int, \
+	is_media int, \
+	inactive int, \
+	hotness_mod int \
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `votes` ( \
+	`id` int NOT NULL PRIMARY KEY, \
+	`OWNER_user_id` int NOT NULL, \
+	`story_id` int NOT NULL, \
+	`comment_id` int, \
+	`vote` int NOT NULL, \
+	`reason` varchar(1) \
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO users VALUES (1, 'admin', 1);
 INSERT INTO users VALUES (2, 'joe', 0);
