8712acd663f2c2288e2a96611933aecd28e4c17a
Kinan Dak Albab <kinan.bab.14@gmail.com>
2021-04-23 19:17:47 -0400

    Clean up duplicated code from rebase

--- bin/drop.sh
+++ bin/drop.sh
@@ -14,10 +14,9 @@ mysql -u $1 -p$2 --execute="DROP DATABASE patients_98f13708210194c475687be6106a3
 mysql -u $1 -p$2 --execute="DROP DATABASE Users_c81e728d9d4c2f636f067f89cc14862c"
 mysql -u $1 -p$2 --execute="DROP DATABASE Users_c4ca4238a0b923820dcc509a6f75849b"
 mysql -u $1 -p$2 --execute="DROP DATABASE Users_eccbc87e4b5ce2fe28308fd9f2a7baf3"
-mysql -u $1 -p$2 --execute="DROP DATABASE users_c81e728d9d4c2f636f067f89cc14862c"
-mysql -u $1 -p$2 --execute="DROP DATABASE users_c4ca4238a0b923820dcc509a6f75849b"
 
 # Lobsters
 mysql -u $1 -p$2 --execute="DROP DATABASE users_c81e728d9d4c2f636f067f89cc14862c"
 mysql -u $1 -p$2 --execute="DROP DATABASE users_c4ca4238a0b923820dcc509a6f75849b"
+
 exit 0
