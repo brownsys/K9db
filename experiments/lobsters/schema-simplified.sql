051ea4d2b5efa08bede5346da761c3b4bb307c9a
Malte Schwarzkopf <malte@cs.brown.edu>
2021-04-24 22:11:28 -0400

    Split queries from Lobsters schema

--- experiments/lobsters/schema-simplified.sql
+++ experiments/lobsters/schema-simplified.sql
@@ -190,13 +190,3 @@ CREATE TABLE votes ( \
   reason varchar(1), \
   FOREIGN KEY (user_id) REFERENCES users(id) \
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE VIEW comments_view AS '"SELECT id FROM comments"';
-
-INSERT INTO users VALUES (1, 'admin', 1);
-INSERT INTO users VALUES (2, 'joe', 0);
-
-INSERT INTO comments VALUES (1, '2021-04-21 01:00:00', NULL, 'asdf', 1, 2, NULL, NULL, 'yo', 0, 0, 0, 0, 0, 0, 0, 0);
-INSERT INTO comments VALUES (2, '2021-04-21 01:00:00', NULL, 'asdf2', 2, 1, NULL, NULL, 'yo2', 0, 0, 0, 0, 0, 0, 0, 0);
-
-SELECT * FROM comments_view;
