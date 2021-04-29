5ed68fab181d2df28fdaafe84c1bf40a2f3e6725
Kinan Dak Albab <kinan.bab.14@gmail.com>
2021-04-29 19:46:49 -0400

    Update GDPRbench

--- experiments/scripts/clear-db.sh
+++ experiments/scripts/clear-db.sh
@@ -8,6 +8,11 @@ mysql -u root -ppassword --execute="DROP DATABASE main_2541acf6383da54e4044f9d3a
 mysql -u root -ppassword --execute="DROP DATABASE main_58e42b48502f2ab9793be0e226dcf67d;"
 mysql -u root -ppassword --execute="DROP DATABASE main_f8d4ae448b914fe9473e92f5d934951e;"
 
+mysql -u root -ppassword --execute="DROP DATABASE main_1793660328a0ee16947af91bdd55e326;"
+mysql -u root -ppassword --execute="DROP DATABASE main_bb02badeb43855f5478559612080fc77;"
+mysql -u root -ppassword --execute="DROP DATABASE main_cdd011eeb3e087fc1471b567d538438c;"
+mysql -u root -ppassword --execute="DROP DATABASE main_d9d8e79f38096c8d3ae4c1d3ce249c3a;"
+
 sleep 3
 
 exit 0
