e51dde8b0f042a9dff9de2d50b1755399571c1af
Benjamin Kilimnik <benjaminkilimnik@gmail.com>
2021-05-03 21:42:37 +0000

    Fix q12, expected query mismatch

--- bin/data/lobsters_queries_expected_mysql.sql
+++ bin/data/lobsters_queries_expected_mysql.sql
@@ -80,7 +80,7 @@ CREATE VIEW q11 AS SELECT stories.id FROM stories WHERE stories.merged_story_id 
 SELECT * FROM q11;
 
 -- ORDER BY
-CREATE VIEW q12 AS SELECT comments.id, comments.created_at, comments.updated_at, comments.short_id, comments.story_id, comments.user_id, comments.parent_comment_id, comments.thread_id, comments.comment, comments.upvotes, comments.downvotes, comments.confidence, comments.markeddown_comment, comments.is_deleted, comments.is_moderated, comments.is_from_email, comments.hat_id, comments.upvotes - comments.downvotes AS saldo FROM comments WHERE comments.story_id = 2 ORDER BY saldo ASC, confidence DESC;
+CREATE VIEW q12 AS SELECT comments.id, comments.created_at, comments.updated_at, comments.short_id, comments.story_id, comments.user_id, comments.parent_comment_id, comments.thread_id, comments.comment, comments.upvotes, comments.downvotes, comments.confidence, comments.markeddown_comment, comments.is_deleted, comments.is_moderated, comments.is_from_email, comments.hat_id, comments.upvotes - comments.downvotes AS saldo FROM comments WHERE comments.story_id = 0 ORDER BY saldo ASC, confidence DESC;
 SELECT * FROM q12;
 -- JOIN
 CREATE VIEW q13 AS SELECT tags.id, tags.tag, tags.description, tags.privileged, tags.is_media, tags.inactive, tags.hotness_mod FROM tags INNER JOIN taggings ON tags.id = taggings.tag_id WHERE taggings.story_id = 0;
