c6879e502bad0fc6e37bc4d483c593b50c8bed5b
Malte Schwarzkopf <malte@cs.brown.edu>
2021-05-03 16:46:08 -0400

    Flip order of join conditions to work around bug
    Q6, Q22 now fail with the schema type mismatch issue that
    Q12 and Q35 encounter as well.

--- bin/data/lobster_q6.sql
+++ bin/data/lobster_q6.sql
@@ -1,2 +1,2 @@
-CREATE VIEW q6 AS '"SELECT comments.upvotes, comments.downvotes FROM comments JOIN stories ON stories.id = comments.story_id WHERE comments.story_id = 0 AND comments.user_id != stories.user_id"';
+CREATE VIEW q6 AS '"SELECT comments.upvotes, comments.downvotes FROM comments JOIN stories ON comments.story_id = stories.id WHERE comments.story_id = 0 AND comments.user_id != stories.user_id"';
 SELECT * FROM q6;
