064bf565a4c1f180047b754bb990afe7c300c4af
Malte Schwarzkopf <malte@cs.brown.edu>
2021-05-07 01:32:59 -0400

    Enable big replying_comments view and add q37

--- experiments/lobsters/views.sql
+++ experiments/lobsters/views.sql
@@ -1,30 +1,29 @@
---needs column/column filter, left join
---CREATE VIEW replying_comments_for_count AS \
---	'"SELECT read_ribbons.user_id, read_ribbons.story_id, comments.id \
---	FROM read_ribbons \
---	JOIN stories ON (stories.id = read_ribbons.story_id) \
---	JOIN comments ON (comments.story_id = read_ribbons.story_id) \
---	LEFT JOIN comments AS parent_comments \
---	ON (parent_comments.id = comments.parent_comment_id) \
---	WHERE read_ribbons.is_following = 1 \
---	AND comments.user_id <> read_ribbons.user_id \
---	AND comments.is_deleted = 0 \
---	AND comments.is_moderated = 0 \
---	AND ( comments.upvotes - comments.downvotes ) >= 0 \
---	AND read_ribbons.updated_at < comments.created_at \
---	AND ( \
---     ( \
---            parent_comments.user_id = read_ribbons.user_id \
---            AND \
---            ( parent_comments.upvotes - parent_comments.downvotes ) >= 0 \
---     ) \
---     OR \
---     ( \
---            parent_comments.id IS NULL \
---            AND \
---            stories.user_id = read_ribbons.user_id \
---     ) \
---     )"';
+CREATE VIEW replying_comments_for_count AS \
+	'"SELECT read_ribbons.user_id, read_ribbons.story_id, comments.id \
+	FROM read_ribbons \
+	JOIN stories ON (read_ribbons.story_id = stories.id) \
+	JOIN comments ON (read_ribbons.story_id = comments.story_id) \
+	LEFT JOIN comments AS parent_comments \
+	ON (comments.parent_comment_id = parent_comments.id) \
+	WHERE read_ribbons.is_following = 1 \
+	AND comments.user_id <> read_ribbons.user_id \
+	AND comments.is_deleted = 0 \
+	AND comments.is_moderated = 0 \
+	AND ( comments.upvotes - comments.downvotes ) >= 0 \
+	AND read_ribbons.updated_at < comments.created_at \
+	AND ( \
+     ( \
+            parent_comments.user_id = read_ribbons.user_id \
+            AND \
+            ( parent_comments.upvotes - parent_comments.downvotes ) >= 0 \
+     ) \
+     OR \
+     ( \
+            parent_comments.id IS NULL \
+            AND \
+            stories.user_id = read_ribbons.user_id \
+     ) \
+     )"';
 CREATE VIEW q1 AS '"SELECT 1 AS `one`, PII_username FROM users WHERE users.PII_username = ?"';
 CREATE VIEW q2 AS '"SELECT 1 AS `one`, short_id FROM stories WHERE stories.short_id = ?"';
 CREATE VIEW q3 AS '"SELECT tags.* FROM tags WHERE tags.inactive = 0 AND tags.tag = ?"';
@@ -66,3 +65,4 @@ CREATE VIEW q34 AS '"SELECT comments.* FROM comments WHERE comments.short_id = ?
 CREATE VIEW q35 AS '"SELECT stories.*, upvotes - downvotes AS saldo FROM stories WHERE stories.merged_story_id IS NULL AND stories.is_expired = 0 ORDER BY id DESC LIMIT 51"';
 --needs the big matview
 --CREATE VIEW q36 AS '"SELECT COUNT(*) FROM replying_comments_for_count WHERE replying_comments_for_count.user_id = ?"';
+CREATE VIEW q37 AS '"SELECT users.* FROM users WHERE users.id = ?"';
