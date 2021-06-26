d58e620d762d64202748725274a6fa123f2e2e16
benkilimnik <benjaminkilimnik@gmail.com>
2021-06-26 04:56:55 +0000

    update print statements, refactor

--- mysql_proxy/src/cpp_wrapper/open.cpp
+++ mysql_proxy/src/cpp_wrapper/open.cpp
@@ -8,8 +8,9 @@
 
 ConnectionC create()
 {
+    std::cout << "C-Wrapper: creating new pelton::Connection..." << std::endl;
     ConnectionC c_conn = {new pelton::Connection()};
-    std::cout << c_conn.cpp_conn << std::endl;
+    std::cout << "C-Wrapper: cpp_conn is stored at " << c_conn.cpp_conn << std::endl;
     return c_conn;
 }
 
@@ -21,11 +22,11 @@ void destroy(ConnectionC c_conn)
 // wrapper that calls c++ from c
 bool open_c(char *db_dir, char *db_username, char *db_password, ConnectionC c_conn)
 {
-    std::cout << "hello?" << std::endl;
-    std::cout << std::string(db_dir) << std::endl;
-    std::cout << std::string(db_username) << std::endl;
-    std::cout << std::string(db_password) << std::endl;
-    std::cout << c_conn.cpp_conn << std::endl;
+    std::cout << "C-Wrapper: starting open_c()..." << std::endl;
+    std::cout << "C-Wrapper: db_dir is: " << std::string(db_dir) << std::endl;
+    std::cout << "C-Wrapper: db_username is: " << std::string(db_username) << std::endl;
+    std::cout << "C-Wrapper: db_passwored is: " << std::string(db_password) << std::endl;
+    std::cout << "C-Wrapper: cpp_conn is: " << c_conn.cpp_conn << std::endl;
     // convert void pointer of ConnectionC struct into c++ class instance Connection 
     pelton::Connection *cpp_conn =
         reinterpret_cast<pelton::Connection *>(c_conn.cpp_conn);
@@ -36,9 +37,10 @@ bool open_c(char *db_dir, char *db_username, char *db_password, ConnectionC c_co
     const std::string c_db_password(db_password);
 
     // call c++ function from C with converted types
+    std::cout << "C-Wrapper: running pelton::open..." << std::endl;
     bool response = pelton::open(db_dir, db_username, db_password, cpp_conn);
     
-    std::cout << "hello again!" << response << std::endl;
+    std::cout << "C-Wrapper: C++ response from pelton is: " << response << std::endl;
 
     return response;
 }
--- mysql_proxy/src/cpp_wrapper/open_main.c
+++ mysql_proxy/src/cpp_wrapper/open_main.c
@@ -3,7 +3,7 @@
 int main(int argc, char **argv)
 {
 	ConnectionC c_conn = create();
-  open_c("", "root", "password", c_conn);
+	open_c("", "root", "password", c_conn);
 	destroy(c_conn);
 	return 0;
 }
