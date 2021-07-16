04ed36a7630837b7390161d4da99f4488bf5d908
ishanSharma07 <mailmeishan007@gmail.com>
2021-07-16 23:18:47 +0000

    Add method for checking whether input is specified or not

--- pelton/dataflow/batch_message.h
+++ pelton/dataflow/batch_message.h
@@ -29,6 +29,8 @@ class BatchMessage : public Message {
         records_(std::move(records)) {}
 
   std::vector<Record> ConsumeRecords() { return std::move(this->records_); }
+  bool ContainsInput() { return this->input_name_; }
+  
   // Accessors
   const std::string &input_name() {
     assert(this->input_name_);
