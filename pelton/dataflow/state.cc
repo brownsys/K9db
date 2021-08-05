027e04b1f1614715712d842523b075daae8152f4
ishanSharma07 <mailmeishan007@gmail.com>
2021-08-05 17:11:46 +0000

    Bug fix
    The record set has to be copied if multiple flows per input are present

--- pelton/dataflow/state.cc
+++ pelton/dataflow/state.cc
@@ -339,13 +339,29 @@ Record DataFlowState::CreateRecord(const sqlast::Insert &insert_stmt) const {
 void DataFlowState::ProcessRecords(const TableName &table_name,
                                    std::vector<Record> &&records) {
   if (records.size() > 0 && this->HasFlowsFor(table_name)) {
-    for (const FlowName &name : this->flows_per_input_table_.at(table_name)) {
+    // One copy of records is required per flow since the records are
+    // dispatched as BatchMessages to the flows.
+    std::vector<std::vector<Record>> records_per_flow;
+    for (size_t i = 0;
+         i < this->flows_per_input_table_.at(table_name).size() - 1; i++) {
+      std::vector<Record> records_copy;
+      for (size_t j = 0; j < records.size(); j++) {
+        records_copy.push_back(records.at(j).Copy());
+      }
+      records_per_flow.push_back(std::move(records_copy));
+    }
+    // Move the original set at the end. This saves one round of copying.
+    records_per_flow.push_back(std::move(records));
+
+    for (size_t i = 0; i < this->flows_per_input_table_.at(table_name).size();
+         i++) {
+      const FlowName &name = this->flows_per_input_table_.at(table_name).at(i);
       // Partition records based on key specified by the input operator
       auto input_partition_key =
           this->input_partitioned_by_.at(name).at(table_name);
       absl::flat_hash_map<uint16_t, std::vector<Record>> partitioned_records =
-          partition::HashPartition(std::move(records), input_partition_key,
-                                   this->partition_count_);
+          partition::HashPartition(std::move(records_per_flow.at(i)),
+                                   input_partition_key, this->partition_count_);
       // Send batch messages to appropriate partitions
       auto flow = this->flows_.at(name);
       for (auto &item : partitioned_records) {
