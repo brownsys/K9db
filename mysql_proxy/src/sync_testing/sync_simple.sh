9f59a1f0813551f3b82dab5ba34bf79889d384eb
bkilimni <benjamin_kilimnik@brown.edu>
2021-10-11 17:34:06 -0400

    refactor proxy multithreading tests

--- mysql_proxy/src/sync_testing/sync_simple.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mariadb --host=127.0.0.1 --port=10001 < social_chat_schema.sql &&
-sleep 5
-mariadb --host=127.0.0.1 --port=10001 < social_chat_inserts.sql &
-mariadb --host=127.0.0.1 --port=10001 < social_chat_queries_simple.sql
-# add parallelism &
-# add more inserts and more queries
-# 1000 inserts 
-# ignore data flow, don't use views
