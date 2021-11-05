6d058762f01ca96ef1cf05f080e9626ae2aaf03b
Benjamin Kilimnik <benjaminkilimnik@gmail.com>
2021-11-04 20:35:01 -0400

    update benchmark scripts

--- bin/sync_testing/sharder_tests/benchmark/sharder_baseline.sh
+++ bin/sync_testing/sharder_tests/benchmark/sharder_baseline.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+./bin/drop.sh root password
+
+# start proxy
+bazel run //mysql_proxy/src:mysql_proxy --config=opt &
+sleep 20
+
+# create tables
+mariadb --host=127.0.0.1 --port=10001 < creates.sql
+sleep 10
+
+# create student shards (by inserting submissions)
+mariadb --host=127.0.0.1 --port=10001 < inserts_new_shard.sql
+sleep 1000
+
+# single threaded inserts to existing submission shards plus 
+# students and assignments that don't have submissions (no new shards created)
 mariadb --host=127.0.0.1 --port=10001 < inserts_existing_shard_all.sql
-# ./drop-db.sh root password
--- bin/sync_testing/sharder_tests/benchmark/sharder_benchmark.sh
+++ bin/sync_testing/sharder_tests/benchmark/sharder_benchmark.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+../../drop-db.sh root password
+
+# start proxy
+bazel run //mysql_proxy/src:mysql_proxy --config=opt &
+sleep 20
+
+# create tables
+mariadb --host=127.0.0.1 --port=10001 < creates.sql
+sleep 10
+
+# create student shards (by inserting submissions)
+mariadb --host=127.0.0.1 --port=10001 < inserts_new_shard.sql
+sleep 1000
+
+# benchmark inserting into existing shards, as well as adding new 
+# students and assignments without submissions (so no new shards created)
 mariadb --host=127.0.0.1 --port=10001 < inserts_existing_shard1.sql &
 mariadb --host=127.0.0.1 --port=10001 < inserts_existing_shard2.sql &
 mariadb --host=127.0.0.1 --port=10001 < inserts_existing_shard3.sql &
 mariadb --host=127.0.0.1 --port=10001 < inserts_existing_shard4.sql 
-# ./drop-db.sh root password
