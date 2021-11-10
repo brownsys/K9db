d1bdac2e4d4fe3e795835d62a10e6e126ab8fce5
Benjamin Kilimnik <90570480+qtmtg66rrjwpndvn@users.noreply.github.com>
2021-11-10 14:56:19 -0500

    add index creation via SQL

--- pelton/shards/sqlengine/engine.cc
+++ pelton/shards/sqlengine/engine.cc
@@ -82,9 +82,12 @@ absl::StatusOr<sql::SqlResult> Shard(const std::string &sql,
       return sql::SqlResult(true);
     }
 
-    // Case 7: CREATE INEDX statement.
+    // Case 7: CREATE INDEX statement.
     case sqlast::AbstractStatement::Type::CREATE_INDEX: {
       auto *stmt = static_cast<sqlast::CreateIndex *>(statement.get());
+      std::cout << "CREATE INDEX NAME " << stmt->index_name() << std::endl;
+      std::cout << "CREATE INDEX COLUMN " << stmt->column_name() << std::endl;
+      std::cout << "CREATE INDEX TABLE " << stmt->table_name() << std::endl;
       return index::CreateIndex(*stmt, state, dataflow_state);
     }
 
