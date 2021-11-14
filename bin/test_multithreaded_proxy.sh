407718a0e9ffe180e460f4208c13c81c64ef9b5f
Benjamin Kilimnik <benjaminkilimnik@gmail.com>
2021-11-13 21:37:19 -0500

    refactor multithreaded E2E tests, adding comments

--- bin/test_multithreaded_proxy.sh
+++ bin/test_multithreaded_proxy.sh
@@ -1,40 +1,7 @@
 # #!/bin/bash
 ./bin/drop.sh root password
 
-# # test lobsters (dataflow)
-bazel run --run_under "valgrind --error-exitcode=1 --errors-for-leak-kinds=definite --leak-check=full --show-leak-kinds=definite" //mysql_proxy/src:mysql_proxy -- -logtostderr=1 &
-sleep 20
-proxy_pid=$!
-mariadb --port=10001 --host=127.0.0.1 < bin/sync_testing/proxy_tests/lobsters_schema_simplified.sql &&
-mariadb --port=10001 --host=127.0.0.1 < bin/sync_testing/proxy_tests/lobsters_views.sql &&
-sleep 30
-mariadb --port=10001 --host=127.0.0.1 < bin/sync_testing/proxy_tests/lobsters_inserts1.sql &
-mariadb --port=10001 --host=127.0.0.1 < bin/sync_testing/proxy_tests/lobsters_inserts2.sql 
-mariadb --port=10001 --host=127.0.0.1 < bin/sync_testing/proxy_tests/lobsters_queries1.sql &
-mariadb --port=10001 --host=127.0.0.1 < bin/sync_testing/proxy_tests/lobsters_queries2.sql
-sleep 60
-client_pid=$!
-kill $client_pid
-kill $proxy_pid
-./bin/drop.sh root password
-
-# # test lobsters and social chat (dataflow)
-bazel run --run_under "valgrind --error-exitcode=1 --errors-for-leak-kinds=definite --leak-check=full --show-leak-kinds=definite" //mysql_proxy/src:mysql_proxy -- -logtostderr=1 &
-sleep 20
-proxy_pid=$!
-mariadb --port=10001 --host=127.0.0.1 < bin/sync_testing/proxy_tests/lobsters_schema_simplified.sql &&
-mariadb --port=10001 --host=127.0.0.1 < bin/sync_testing/proxy_tests/social_chat_schema.sql &&
-mariadb --port=10001 --host=127.0.0.1 < bin/sync_testing/proxy_tests/lobsters_views.sql &&
-sleep 30
-mariadb --port=10001 --host=127.0.0.1 < bin/sync_testing/proxy_tests/lobsters_inserts.sql &
-mariadb --port=10001 --host=127.0.0.1 < bin/sync_testing/proxy_tests/lobsters_queries.sql
-sleep 60
-client_pid=$!
-kill $client_pid
-kill $proxy_pid
-./bin/drop.sh root password
-
-# test concurrent creates
+# E2E test for concurrent creates
 bazel run --run_under "valgrind --error-exitcode=1 --errors-for-leak-kinds=definite --leak-check=full --show-leak-kinds=definite" //mysql_proxy/src:mysql_proxy -- -logtostderr=1 &
 sleep 20
 proxy_pid=$!
@@ -46,7 +13,7 @@ kill $client_pid
 kill $proxy_pid
 ./bin/drop.sh root password
 
-# # test concurrent inserts
+# E2E test for concurrent inserts
 bazel run --run_under "valgrind --error-exitcode=1 --errors-for-leak-kinds=definite --leak-check=full --show-leak-kinds=definite" //mysql_proxy/src:mysql_proxy -- -logtostderr=1 &
 sleep 20
 proxy_pid=$!
@@ -60,3 +27,20 @@ client_pid=$!
 kill $client_pid
 kill $proxy_pid
 ./bin/drop.sh root password
+
+# # E2E test for multi-threaded inserts and view queries from lobsters (TODO: uncomment after merging multi-threaded dataflow engine)
+# bazel run --run_under "valgrind --error-exitcode=1 --errors-for-leak-kinds=definite --leak-check=full --show-leak-kinds=definite" //mysql_proxy/src:mysql_proxy -- -logtostderr=1 &
+# sleep 20
+# proxy_pid=$!
+# mariadb --port=10001 --host=127.0.0.1 < bin/sync_testing/proxy_tests/lobsters_schema_simplified.sql &&
+# mariadb --port=10001 --host=127.0.0.1 < bin/sync_testing/proxy_tests/lobsters_views.sql &&
+# sleep 30
+# mariadb --port=10001 --host=127.0.0.1 < bin/sync_testing/proxy_tests/lobsters_inserts1.sql &
+# mariadb --port=10001 --host=127.0.0.1 < bin/sync_testing/proxy_tests/lobsters_inserts2.sql 
+# mariadb --port=10001 --host=127.0.0.1 < bin/sync_testing/proxy_tests/lobsters_queries1.sql &
+# mariadb --port=10001 --host=127.0.0.1 < bin/sync_testing/proxy_tests/lobsters_queries2.sql
+# sleep 60
+# client_pid=$!
+# kill $client_pid
+# kill $proxy_pid
+# ./bin/drop.sh root password
