0457fb3ebc81de3bfa590cdaef839b637d9d4468
Benjamin Kilimnik <benjamin_kilimnik@brown.edu>
2021-11-18 15:48:57 -0500

    resolve create.cc comment merge conflict

--- pelton/shards/sqlengine/create.cc
+++ pelton/shards/sqlengine/create.cc
@@ -256,11 +256,7 @@ void IndexAccessor(const sqlast::CreateTable &stmt, SharderState &state,
       // terminate called after throwing an instance of
       // 'sql::SQLSyntaxErrorException' what():  (conn=90) Identifier name
       // 'pd3d9446802a44259755d38e6_r_doctors_chat_ACCESSOR_doctor_id_OWNER_patient_id'
-<<<<<<< HEAD
-      // is too long
-=======
       // is too long (when including column name)
->>>>>>> fb90fe1fc2dd6031a477cf854fcb6e08ab4402c3
       sqlast::CreateIndex create_index_stmt{
           "r_" + shard_string + "_" + table_name, table_name, column_name};
       std::cout << "Starting CreateIndex: " << std::endl;
