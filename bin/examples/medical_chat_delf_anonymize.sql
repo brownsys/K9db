c46b358dec4e42076bf78b34ba19b8ea0756d7ff
Benjamin Kilimnik <benjamin_kilimnik@brown.edu>
2021-12-01 16:19:33 -0500

    add tests for on delete retain and on delete anonymize

---
+++ bin/examples/medical_chat_delf_anonymize.sql
@@ -0,0 +1,51 @@
+SET echo;
+
+CREATE TABLE doctors ( \
+  id int, \
+  PII_name text, \
+  PRIMARY KEY(id) \
+);
+
+CREATE TABLE patients ( \
+  id int, \
+  PII_name text, \
+  PRIMARY KEY(id) \
+);
+
+CREATE TABLE chat ( \
+  id int, \
+  OWNER_patient_id int, \
+  ACCESSOR_ANONYMIZE_doctor_id int, \
+  message text, \
+  PRIMARY KEY(id), \
+  FOREIGN KEY (OWNER_patient_id) REFERENCES patients(id), \
+  FOREIGN KEY (ACCESSOR_ANONYMIZE_doctor_id) REFERENCES doctors(id) \
+);
+
+INSERT INTO doctors VALUES (1, 'Alice');
+INSERT INTO doctors(id, PII_name) VALUES (2, 'Bob');
+
+INSERT INTO patients VALUES (10, 'Carl');
+INSERT INTO patients VALUES (20, 'Dracula');
+
+INSERT INTO chat VALUES (1, 10, 1, 'HELLO');
+INSERT INTO chat VALUES (2, 10, 2, 'Good bye');
+INSERT INTO chat VALUES (3, 20, 1, 'HELLO');
+INSERT INTO chat VALUES (4, 20, 2, 'HELLO 2');
+INSERT INTO chat VALUES (5, 20, 1, 'HELLO 3');
+
+GDPR FORGET doctors 2;
+SELECT * FROM chat;
+
+-- SELECT * FROM chat;
+-- SELECT * FROM chat WHERE ACCESSOR_doctor_id = 2;
+-- SELECT * FROM chat WHERE OWNER_patient_id = 10;
+-- GDPR GET patients 10;
+-- GDPR GET doctors 2;
+
+-- SELECT * FROM chat;
+
+-- GDPR FORGET patients 10;
+-- SELECT * FROM patients;
+-- SELECT * FROM chat;
+-- SELECT * FROM ref_doctors_OWNER_patient_id;
