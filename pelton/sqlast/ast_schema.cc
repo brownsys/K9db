041d5b521d612d475b4d2fb41b92bc58807abe72
Justus Adam <dev@justus.science>
2021-12-03 19:52:22 +0000

    Accept `INTEGER` as type

--- pelton/sqlast/ast_schema.cc
+++ pelton/sqlast/ast_schema.cc
@@ -48,7 +48,7 @@ std::string &ColumnConstraint::foreign_column() {
 // ColumnDefinition
 ColumnDefinition::Type ColumnDefinition::StringToType(
     const std::string &column_type) {
-  if (absl::EqualsIgnoreCase(column_type, "INT")) {
+  if (absl::EqualsIgnoreCase(column_type, "INT") || absl::EqualsIgnoreCase(column_type, "INTEGER")) {
     return Type::INT;
   } else if (absl::StartsWithIgnoreCase(column_type, "VARCHAR") ||
              absl::EqualsIgnoreCase(column_type, "TEXT")) {
