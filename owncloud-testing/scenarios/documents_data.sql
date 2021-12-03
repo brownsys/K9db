5e006b1e162ce02949cf71925afc7ac0c557fb4b
Justus Adam <dev@justus.science>
2021-12-03 21:22:33 +0000

    Simple file sharing without owner done

--- owncloud-testing/scenarios/documents_data.sql
+++ owncloud-testing/scenarios/documents_data.sql
@@ -1,6 +1,12 @@
 INSERT INTO oc_users (uid, PII_displayname, password) VALUES ('claire', 'Claire', 'password');
 INSERT INTO oc_users (uid, PII_displayname, password) VALUES ('wong', 'Wong', 'password');
 
+
 INSERT INTO oc_share 
-    (id, share_type, OWNER_share_with, uid_owner, uid_initiator, parent, item_type, item_source, item_target, file_target, permissions, stime, accepted, expiration, token, mail_send, share_name, file_source, attributes)
-VALUES (0, 0, 'wong', 'claire', 'claire', NULL, 'file', '/books/The Sorcerers Stone.ebook', '', '', 24, 0, 0, NULL, '', 1, '', 24, 19);
+    --(id, share_type, OWNER_share_with, uid_owner, uid_initiator, parent, item_type, OWNING_item_source, item_target, file_target, permissions, stime, accepted, expiration, token, mail_send, share_name, file_source, attributes)
+VALUES (0, 0, 'wong', 'claire', 'claire', NULL, 'file', 0, '', '', 24, 0, 0, NULL, '', 1, '', 24, 19);
+INSERT INTO oc_share 
+VALUES (1, 0, 'claire', 'wong', 'wong', NULL, 'file', 1, '', '', 24, 0, 0, NULL, '', 1, '', 24, 19);
+
+INSERT INTO oc_files (id, file_name, owner) VALUES (0, '/books/The Sorcerers Stone.ebook', 'claire');
+INSERT INTO oc_files (id, file_name, owner) VALUES (1, '/books/The Chamber of Secrets.ebook', 'wong');
