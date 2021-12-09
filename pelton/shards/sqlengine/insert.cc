b2f1948c92cc3a1d6e909f09fc9a0cb7380e203c
Justus Adam <dev@justus.science>
2021-12-09 21:16:36 +0000

    Dropping some log statements

--- pelton/shards/sqlengine/insert.cc
+++ pelton/shards/sqlengine/insert.cc
@@ -203,7 +203,6 @@ absl::StatusOr<sql::SqlResult> Shard(const sqlast::Insert &stmt,
   if (!is_sharded) {
     // Case 1: table is not in any shard.
     // The insertion statement is unmodified.
-    LOG(INFO) << "Table " << table_name << " is not sharded.";
     result = exec.ExecuteDefault(&stmt);
 
   } else {  // is_sharded == true
@@ -211,21 +210,18 @@ absl::StatusOr<sql::SqlResult> Shard(const sqlast::Insert &stmt,
     // Duplicate the value for every shard this table has.
     for (const ShardingInformation &info :
          state->GetShardingInformation(table_name)) {
-      LOG(INFO) << "Handling shard " << info.sharded_table_name;
       sqlast::Insert cloned = stmt;
       cloned.table_name() = info.sharded_table_name;
       // Find the value corresponding to the shard by column.
       std::string user_id;
       if (cloned.HasColumns()) {
         if (info.IsTransitive()) {
-          LOG(INFO) << "Table was found to be transitive and have columns";
           ASSIGN_OR_RETURN(user_id, cloned.GetValue(info.shard_by));
         } else {
           ASSIGN_OR_RETURN(user_id, cloned.RemoveValue(info.shard_by));
         }
       } else {
         if (info.IsTransitive()) {
-          LOG(INFO) << "Table was found to be transitive";
           user_id = cloned.GetValue(info.shard_by_index);
         } else {
           user_id = cloned.RemoveValue(info.shard_by_index);
@@ -238,7 +234,6 @@ absl::StatusOr<sql::SqlResult> Shard(const sqlast::Insert &stmt,
         continue;
       }
       user_id = Dequote(user_id);
-      LOG(INFO) << "user_id dequoted";
 
       // If the sharding is transitive, the user id should be resolved via the
       // secondary index of the target table.
@@ -249,19 +244,16 @@ absl::StatusOr<sql::SqlResult> Shard(const sqlast::Insert &stmt,
       // its own function and I'm calling it here either on the original user (else branch) or in a 
       // loop for each of the variable owners.
       if (info.IsTransitive()) {
-        LOG(INFO) << "Looking up using " << info.next_index_name;
         ASSIGN_OR_RETURN(
             auto &lookup,
             index::LookupIndex(info.next_index_name, user_id, dataflow_state));
         if (lookup.size() < 1) {
-          LOG(INFO) << "Lookup result was empty";
           continue;
         } else {
-          LOG(INFO) << "Index lookup succeeded";
           for (auto &uid : lookup) {
-            LOG(INFO) << "one lookup result was null";
             if (!absl::EqualsIgnoreCase(uid, "NULL")) {
-            CHECK_STATUS(HandleShardForUser(stmt, cloned, connection, lock, &update_flows, &result, info, uid));
+              CHECK_STATUS(HandleShardForUser(stmt, cloned, connection, lock, &update_flows, &result, info, uid));
+            }
           }
         }
       } else {
