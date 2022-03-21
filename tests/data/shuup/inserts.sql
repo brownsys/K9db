bdae925b999a9a0a469e1cbbb5a5085dba3e47b3
Benjamin Kilimnik <benjamin_kilimnik@brown.edu>
2022-03-21 18:49:52 -0400

    update insert order for OWNING debugging

--- tests/data/shuup/inserts.sql
+++ tests/data/shuup/inserts.sql
@@ -1,10 +1,10 @@
-INSERT INTO shuup_contact(id, PII_name) VALUES (1, 'Alice');
-INSERT INTO shuup_contact(id, PII_name) VALUES (2, 'Banji');
-INSERT INTO shuup_contact(id, PII_name) VALUES (3, 'Keenan');
+INSERT INTO shuup_personcontact VALUES (1, 'Alice', 10, "email@email.com");
+INSERT INTO shuup_personcontact VALUES (2, 'Banji', 20, "banj@gmail.com");
+INSERT INTO shuup_personcontact VALUES (3, 'Keenan', 0, "keenan@hotmail.com");
 
 INSERT INTO auth_user VALUES (10, 'Alice', 'secure');
 INSERT INTO auth_user VALUES (20, 'Banji', 'password');
 
-INSERT INTO shuup_personcontact VALUES (1, 'Alice', 10, "email@email.com");
-INSERT INTO shuup_personcontact VALUES (2, 'Banji', 20, "banj@gmail.com");
-INSERT INTO shuup_personcontact VALUES (3, 'Keenan', 0, "keenan@hotmail.com");
+INSERT INTO shuup_contact(id, name) VALUES (1, 'Alice');
+INSERT INTO shuup_contact(id, name) VALUES (2, 'Banji');
+INSERT INTO shuup_contact(id, name) VALUES (3, 'Keenan');
