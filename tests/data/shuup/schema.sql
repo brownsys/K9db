ac8e39430bcab4e91929d737c84b2d08404e8bd6
Benjamin Kilimnik <benjamin_kilimnik@brown.edu>
2022-03-21 17:42:31 -0400

    update OWNS schema

--- tests/data/shuup/schema.sql
+++ tests/data/shuup/schema.sql
@@ -1,13 +1,3 @@
-CREATE TABLE shuup_personcontact ( \
-  OWNS_contact_ptr_id int, \
-  PII_name text, \
-  OWNS_user_id int, \
-  email text, \
-  PRIMARY KEY(OWNS_contact_ptr_id) \
-  FOREIGN KEY (OWNS_contact_ptr_id) REFERENCES shuup_contact(id), \
-  FOREIGN KEY (OWNS_user_id) REFERENCES auth_user(id) \
-);
-
 CREATE TABLE shuup_contact ( \
   id int, \
   name text, \
@@ -19,4 +9,14 @@ CREATE TABLE auth_user ( \
   username text, \
   password text, \
   PRIMARY KEY(id) \
+);
+
+CREATE TABLE shuup_personcontact ( \
+  OWNING_contact_ptr_id int, \
+  PII_name text, \
+  OWNING_user_id int, \
+  email text, \
+  PRIMARY KEY(OWNING_contact_ptr_id), \
+  FOREIGN KEY (OWNING_contact_ptr_id) REFERENCES shuup_contact(id), \
+  FOREIGN KEY (OWNING_user_id) REFERENCES auth_user(id) \
 );
