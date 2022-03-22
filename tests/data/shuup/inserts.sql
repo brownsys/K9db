93e22bac86d8062a6a70ae7e4ff4732460954a6b
Aaron Jeyaraj <aaron_jeyaraj@brown.edu>
2022-03-22 15:22:05 -0400

    schema updates

--- tests/data/shuup/inserts.sql
+++ tests/data/shuup/inserts.sql
@@ -1,10 +1,10 @@
-INSERT INTO shuup_contact(id, PII_name) VALUES (1, 'Alice');
-INSERT INTO shuup_contact(id, PII_name) VALUES (2, 'Banji');
-INSERT INTO shuup_contact(id, PII_name) VALUES (3, 'Keenan');
+INSERT INTO shuup_personcontact VALUES (1, 100, 'Alice', 10, "email@email.com");
+INSERT INTO shuup_personcontact VALUES (2, 200, 'Banji', 20, "banj@gmail.com");
+INSERT INTO shuup_personcontact VALUES (3, 300, 'Keenan', 0, "keenan@hotmail.com");
 
-INSERT INTO auth_user VALUES (10, 'Alice', 'secure');
-INSERT INTO auth_user VALUES (20, 'Banji', 'password');
+INSERT INTO shuup_contact(id, ptr, name) VALUES (6, 100, 'Alice');
+INSERT INTO shuup_contact(id, name) VALUES (7, 200, 'Banji');
+INSERT INTO shuup_contact(id, name) VALUES (8, 300, 'Keenan');
 
-INSERT INTO shuup_personcontact VALUES (1, 'Alice', 10, "email@email.com");
-INSERT INTO shuup_personcontact VALUES (2, 'Banji', 20, "banj@gmail.com");
-INSERT INTO shuup_personcontact VALUES (3, 'Keenan', 0, "keenan@hotmail.com");
+INSERT INTO auth_user(id, ptr, username, password) VALUES (15, 10, 'Alice', 'secure');
+INSERT INTO auth_user VALUES (16, 20, 'Banji', 'password');
--- tests/data/shuup/schema.sql
+++ tests/data/shuup/schema.sql
@@ -1,22 +1,29 @@
 CREATE TABLE shuup_contact ( \
   id int, \
+  ptr int, \
   name text, \
   PRIMARY KEY(id) \
 );
 
+CREATE INDEX shuup_contact_ptr ON shuup_contact(ptr);
+
 CREATE TABLE auth_user ( \
   id int, \
+  ptr int, \
   username text, \
   password text, \
   PRIMARY KEY(id) \
 );
 
+CREATE INDEX auth_user_ptr ON auth_user(ptr);
+
 CREATE TABLE shuup_personcontact ( \
+  pid int, \
   OWNING_contact_ptr_id int, \
   PII_name text, \
   OWNING_user_id int, \
   email text, \
-  PRIMARY KEY(OWNING_contact_ptr_id), \
-  FOREIGN KEY (OWNING_contact_ptr_id) REFERENCES shuup_contact(id), \
-  FOREIGN KEY (OWNING_user_id) REFERENCES auth_user(id) \
+  PRIMARY KEY(pid), \
+  FOREIGN KEY (OWNING_contact_ptr_id) REFERENCES shuup_contact(ptr), \
+  FOREIGN KEY (OWNING_user_id) REFERENCES auth_user(ptr) \
 );
