f1bb7069877fd0ef8a81c1ec49033a4b15008a78
Raj Paul <rajdpaul01@gmail.com>
2022-04-04 17:14:03 -0400

    created basic encryption API in encryption.cc

--- pelton/sql/connections/encryption.cc
+++ pelton/sql/connections/encryption.cc
@@ -4,6 +4,49 @@
 // NOLINTNEXTLINE
 #include "sodium.h"
 
+#define MAX_LEN 1024
+
+// a global random string that is the same for all keys and users.
+unsigned char nonce[crypto_aead_aes256gcm_NPUBBYTES];
+
+// generates random 256-bit key
+unsigned char* KeyGen() {
+  static unsigned char key[crypto_aead_aes256gcm_KEYBYTES];
+  crypto_aead_aes256gcm_keygen(key);
+  return key;
+}
+
+// encrypts pt (plaintext) with key and returns ct (ciphertext)
+std::string Encrypt(unsigned char *key, std::string pt) {
+  size_t known_length = pt.size() + crypto_aead_aes256gcm_ABYTES;
+  static unsigned char *ct = new unsigned char[known_length];
+  long long unsigned ct_len = 0;
+  const char *pt_buffer = pt.c_str();
+  auto casted_pt_buffer = reinterpret_cast<const unsigned char *>(pt_buffer);
+
+  crypto_aead_aes256gcm_encrypt(ct, &ct_len, 
+                                casted_pt_buffer, pt.size(), 
+                                nullptr, 0, NULL, nonce, key);
+ 
+  std::string ct_str(reinterpret_cast<char*>(ct));
+  return ct_str;
+}
+
+// decrypts ct (ciphertext) with key and returns pt (plaintext)
+std::string Decrypt(unsigned char *key, std::string ct) {
+  static unsigned char *pt = new unsigned char[MAX_LEN];
+  long long unsigned pt_len = 0;
+  const char *ct_buffer = ct.c_str();
+  auto casted_ct_buffer = reinterpret_cast<const unsigned char *>(ct_buffer);
+  
+  crypto_aead_aes256gcm_decrypt(pt, &pt_len, NULL,
+                                casted_ct_buffer, ct.size(), 
+                                nullptr, 0, nonce, key);
+  
+  std::string pt_str(reinterpret_cast<char*>(pt));
+  return pt_str;
+}
+
 int main(int argc, char **argv) {
   std::string message = "HELLO AES!";
   std::cout << "len: " << message.size() << std::endl;
@@ -11,58 +54,27 @@ int main(int argc, char **argv) {
   std::cout << std::endl;
 
   // Initialize libsodium.
-  sodium_init();
+  if (sodium_init() == -1) {
+    return 1;
+  }
 
-  // Nonce is a global random string that is the same for all keys and users.
-  unsigned char nonce[crypto_aead_aes256gcm_NPUBBYTES];
+  // set nonce
   randombytes_buf(nonce, sizeof nonce);
 
   // Generate key.
-  unsigned char key[crypto_aead_aes256gcm_KEYBYTES];
-  crypto_aead_aes256gcm_keygen(key);
+  unsigned char *key = KeyGen();
 
-  // The encryption size is the same as the input plaintext size + a fixed
-  // encryption overhead defined in crypto_aead_aes256gcm_ABYTES.
-  size_t known_length = message.size() + crypto_aead_aes256gcm_ABYTES;
-  unsigned char *ciphertext = new unsigned char[known_length];
-  // The ciphertext buffer is sometimes fixed-sized ahead of knowing the size of
-  // the plaintext message (so its size is an upperbound). For this reason,
-  // Libsodium API can be given a pointer to a variable where it will write
-  // the actual length of the ciphertext after encryption.
-  // NOLINTNEXTLINE
-  long long unsigned ciphertext_len = 0;
+  // encrypt message
+  std::string ciphertext = Encrypt(key, message);
 
-  // Encrypt.
-  const char *message_buffer = message.c_str();
-  auto casted_buffer = reinterpret_cast<const unsigned char *>(message_buffer);
-  crypto_aead_aes256gcm_encrypt(ciphertext, &ciphertext_len, casted_buffer,
-                                message.size(), nullptr, 0, NULL, nonce, key);
-
-  // Print information.
-  std::cout << "len: " << ciphertext_len << std::endl;
-  std::cout << "len: " << known_length << std::endl;
-  std::cout << "cipher: ";
-  for (uint64_t i = 0; i < ciphertext_len; i++) {
-    std::cout << ciphertext[i];
-  }
-  std::cout << std::endl;
+  // print ciphertext
+  std::cout << "ciphertext: " << ciphertext << std::endl;
   std::cout << std::endl;
 
-  // Decrypt.
-  // In the same way, the decryption buffer can be some fixed-size thing that is
-  // defined ahead of time. The exact decrypted length will be written by
-  // libsodium.
-  char decrypted[1000];
-  auto casted_decrypted = reinterpret_cast<unsigned char *>(decrypted);
-  // NOLINTNEXTLINE
-  long long unsigned decrypted_len = 0;
-  crypto_aead_aes256gcm_decrypt(casted_decrypted, &decrypted_len, NULL,
-                                ciphertext, ciphertext_len, nullptr, 0, nonce,
-                                key);
+  // decrypt message
+  std::string plaintext = Decrypt(key, ciphertext);
 
-  // Print information.
-  std::cout << "len: " << decrypted_len << std::endl;
-  std::cout << "message: " << std::string(decrypted, decrypted_len)
-            << std::endl;
+  // print plaintext
+  std::cout << "plaintext: " << plaintext << std::endl;
   std::cout << std::endl;
 }
