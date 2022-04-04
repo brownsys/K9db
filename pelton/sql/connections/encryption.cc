3b2b144f31b9336705b04c6cc853d7853b766901
Raj Paul <rajdpaul01@gmail.com>
2022-04-04 18:10:06 -0400

    basic test/benchmark for encryption.cc

--- pelton/sql/connections/encryption.cc
+++ pelton/sql/connections/encryption.cc
@@ -1,10 +1,20 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <utility>
+#include <chrono>
+#include <assert.h>
+#include <ctime>
+#include <unistd.h>
 
 // NOLINTNEXTLINE
 #include "sodium.h"
 
 #define MAX_LEN 10000
+
+#define NUM_TESTS 10000
+#define INPUT_SIZE 100
 
 // a global random string that is the same for all keys and users.
 unsigned char nonce[crypto_aead_aes256gcm_NPUBBYTES];
@@ -47,11 +57,22 @@ std::string Decrypt(unsigned char *key, std::string ct) {
   return pt_str;
 }
 
+std::string gen_random_str(const int len) {
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+    std::string tmp_s;
+    tmp_s.reserve(len);
+
+    for (int i = 0; i < len; ++i) {
+        tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
+    }
+    
+    return tmp_s;
+}
+
 int main(int argc, char **argv) {
-  std::string message = "HELLO AES!";
-  std::cout << "len: " << message.size() << std::endl;
-  std::cout << "message: " << message << std::endl;
-  std::cout << std::endl;
 
   // Initialize libsodium.
   if (sodium_init() == -1) {
@@ -64,17 +85,66 @@ int main(int argc, char **argv) {
   // Generate key.
   unsigned char *key = KeyGen();
 
-  // encrypt message
-  std::string ciphertext = Encrypt(key, message);
+  // Timers for recording encryption and decryption times
+  uint64_t encryption_time = 0;
+  uint64_t decryption_time = 0;
 
-  // print ciphertext
-  std::cout << "ciphertext: " << ciphertext << std::endl;
-  std::cout << std::endl;
+  // Encrypt and Decrypt NUM_TESTS random strings
+  std::clog << "Performing encryptions and decryptions...\n";
 
-  // decrypt message
-  std::string plaintext = Decrypt(key, ciphertext);
+  for (int i = 0; i < NUM_TESTS; i++) {
+    std::string line = gen_random_str(INPUT_SIZE);
 
-  // print plaintext
-  std::cout << "plaintext: " << plaintext << std::endl;
-  std::cout << std::endl;
+    // Encrypt line, record time
+    auto encr_start = std::chrono::steady_clock::now();
+    std::string ciphertext = Encrypt(key, line);
+    auto encr_stop = std::chrono::steady_clock::now();
+    encryption_time += std::chrono::duration_cast<std::chrono::nanoseconds>(
+      encr_stop - encr_start).count();
+
+    // Decrypt line, record time
+    auto decr_start = std::chrono::steady_clock::now();
+    std::string plaintext = Decrypt(key, ciphertext);
+    auto decr_stop = std::chrono::steady_clock::now();
+    decryption_time += std::chrono::duration_cast<std::chrono::nanoseconds>(
+      decr_stop - decr_start).count();
+
+    // assert decryption correctness
+    assert(line == plaintext);
+  }
+
+  // Print times for encryptions and decryptions
+  std::cout << 
+  "Encryptions time: " << 
+    static_cast<double>(encryption_time) / 1000000000 << " s\n" 
+  "Decryptions time: " <<
+    static_cast<double>(decryption_time) / 1000000000 << " s\n";
+
+  return 0;
+
+
+
+
+
+
+  // std::string message = "HELLO AES!";
+  // std::cout << "len: " << message.size() << std::endl;
+  // std::cout << "message: " << message << std::endl;
+  // std::cout << std::endl;
+  // // Initialize libsodium.
+  // sodium_init();
+  // // set nonce
+  // randombytes_buf(nonce, sizeof nonce);
+  // // Generate key.
+  // unsigned char *key = KeyGen();
+  // // encrypt message
+  // std::string ciphertext = Encrypt(key, message);
+  // // print ciphertext
+  // std::cout << "ciphertext: " << ciphertext << std::endl;
+  // std::cout << std::endl;
+  // // decrypt message
+  // std::string plaintext = Decrypt(key, ciphertext);
+  // // print plaintext
+  // std::cout << "plaintext: " << plaintext << std::endl;
+  // std::cout << std::endl;
 }
