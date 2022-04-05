a18dbf5bf3a52efc95f33c783b3d9590b00fe768
Raj Paul <rajdpaul01@gmail.com>
2022-04-04 21:25:38 -0400

    turned encryption binary to library

--- pelton/sql/connections/encryption.cc
+++ pelton/sql/connections/encryption.cc
@@ -1,23 +1,25 @@
-#include <fstream>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <utility>
-#include <chrono>
-#include <assert.h>
-#include <ctime>
-#include <unistd.h>
+#include "pelton/sql/connections/encryption.h"
 
-// NOLINTNEXTLINE
-#include "sodium.h"
+#include "glog/logging.h"
+
+namespace pelton {
+namespace sql {
 
 #define MAX_LEN 10000
-
-#define NUM_TESTS 10000
-#define INPUT_SIZE 100
+#define NUM_TESTS 100000
+#define INPUT_SIZE 250
 
 // a global random string that is the same for all keys and users.
 unsigned char nonce[crypto_aead_aes256gcm_NPUBBYTES];
+
+// initializes libsodium and sets nonce
+void InitializeEncryption() {
+  // Initialize libsodium.
+  sodium_init();
+
+  // set nonce
+  randombytes_buf(nonce, sizeof nonce);
+}
 
 // generates random 256-bit key
 unsigned char* KeyGen() {
@@ -57,94 +59,71 @@ std::string Decrypt(unsigned char *key, std::string ct) {
   return pt_str;
 }
 
-std::string gen_random_str(const int len) {
-    static const char alphanum[] =
-        "0123456789"
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz";
-    std::string tmp_s;
-    tmp_s.reserve(len);
+// std::string gen_random_str(const int len) {
+//     static const char alphanum[] =
+//         "0123456789"
+//         "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//         "abcdefghijklmnopqrstuvwxyz";
+//     std::string tmp_s;
+//     tmp_s.reserve(len);
 
-    for (int i = 0; i < len; ++i) {
-        tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
-    }
+//     for (int i = 0; i < len; ++i) {
+//         tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
+//     }
     
-    return tmp_s;
-}
+//     return tmp_s;
+// }
 
-int main(int argc, char **argv) {
+// int main(int argc, char **argv) {
 
-  // Initialize libsodium.
-  if (sodium_init() == -1) {
-    return 1;
-  }
+//   // Initialize libsodium.
+//   if (sodium_init() == -1) {
+//     return 1;
+//   }
 
-  // set nonce
-  randombytes_buf(nonce, sizeof nonce);
+//   // set nonce
+//   randombytes_buf(nonce, sizeof nonce);
 
-  // Generate key.
-  unsigned char *key = KeyGen();
+//   // Generate key.
+//   unsigned char *key = KeyGen();
 
-  // Timers for recording encryption and decryption times
-  uint64_t encryption_time = 0;
-  uint64_t decryption_time = 0;
+//   // Timers for recording encryption and decryption times
+//   uint64_t encryption_time = 0;
+//   uint64_t decryption_time = 0;
 
-  // Encrypt and Decrypt NUM_TESTS random strings
-  std::clog << "Performing encryptions and decryptions...\n";
+//   // Encrypt and Decrypt NUM_TESTS random strings
+//   std::clog << "Performing encryptions and decryptions...\n";
 
-  for (int i = 0; i < NUM_TESTS; i++) {
-    std::string line = gen_random_str(INPUT_SIZE);
+//   for (int i = 0; i < NUM_TESTS; i++) {
+//     std::string line = gen_random_str(INPUT_SIZE);
 
-    // Encrypt line, record time
-    auto encr_start = std::chrono::steady_clock::now();
-    std::string ciphertext = Encrypt(key, line);
-    auto encr_stop = std::chrono::steady_clock::now();
-    encryption_time += std::chrono::duration_cast<std::chrono::nanoseconds>(
-      encr_stop - encr_start).count();
+//     // Encrypt line, record time
+//     auto encr_start = std::chrono::steady_clock::now();
+//     std::string ciphertext = Encrypt(key, line);
+//     auto encr_stop = std::chrono::steady_clock::now();
+//     encryption_time += std::chrono::duration_cast<std::chrono::nanoseconds>(
+//       encr_stop - encr_start).count();
 
-    // Decrypt line, record time
-    auto decr_start = std::chrono::steady_clock::now();
-    std::string plaintext = Decrypt(key, ciphertext);
-    auto decr_stop = std::chrono::steady_clock::now();
-    decryption_time += std::chrono::duration_cast<std::chrono::nanoseconds>(
-      decr_stop - decr_start).count();
+//     // Decrypt line, record time
+//     auto decr_start = std::chrono::steady_clock::now();
+//     std::string plaintext = Decrypt(key, ciphertext);
+//     auto decr_stop = std::chrono::steady_clock::now();
+//     decryption_time += std::chrono::duration_cast<std::chrono::nanoseconds>(
+//       decr_stop - decr_start).count();
 
-    // assert decryption correctness
-    assert(line == plaintext);
-  }
+//     // assert decryption correctness
+//     assert(line == plaintext);
+//   }
 
-  // Print times for encryptions and decryptions
-  std::cout << 
-  "Encryptions time: " << 
-    static_cast<double>(encryption_time) / 1000000000 << " s\n" 
-  "Decryptions time: " <<
-    static_cast<double>(decryption_time) / 1000000000 << " s\n";
+//   // Print times for encryptions and decryptions
+//   std::cout << 
+//   "Encryptions: " << 
+//     static_cast<double>(encryption_time) / 1000000000 << " s, " 
+//   "Decryptions: " <<
+//     static_cast<double>(decryption_time) / 1000000000 << " s\n";
 
-  return 0;
+//   return 0;
+// }
 
-
-
-
-
-
-  // std::string message = "HELLO AES!";
-  // std::cout << "len: " << message.size() << std::endl;
-  // std::cout << "message: " << message << std::endl;
-  // std::cout << std::endl;
-  // // Initialize libsodium.
-  // sodium_init();
-  // // set nonce
-  // randombytes_buf(nonce, sizeof nonce);
-  // // Generate key.
-  // unsigned char *key = KeyGen();
-  // // encrypt message
-  // std::string ciphertext = Encrypt(key, message);
-  // // print ciphertext
-  // std::cout << "ciphertext: " << ciphertext << std::endl;
-  // std::cout << std::endl;
-  // // decrypt message
-  // std::string plaintext = Decrypt(key, ciphertext);
-  // // print plaintext
-  // std::cout << "plaintext: " << plaintext << std::endl;
-  // std::cout << std::endl;
-}
+}  // namespace sql
+}  // namespace pelton
---
+++ pelton/sql/connections/encryption.h
@@ -0,0 +1,31 @@
+#ifndef PELTON_SQL_CONNECTIONS_ENCRYPTION_H__
+#define PELTON_SQL_CONNECTIONS_ENCRYPTION_H__
+
+#include <iostream>
+#include <string>
+#include <utility>
+#include <chrono>
+#include <assert.h>
+
+#include "sodium.h"
+
+namespace pelton {
+namespace sql {
+
+// initializes libsodium and sets nonce
+void InitializeEncryption();
+
+// generates random 256-bit key
+unsigned char* KeyGen();
+
+// encrypts pt (plaintext) with key and returns ct (ciphertext)
+std::string Encrypt(unsigned char *key, std::string pt);
+
+// decrypts ct (ciphertext) with key and returns pt (plaintext)
+std::string Decrypt(unsigned char *key, std::string ct);
+
+}  // namespace sql
+}  // namespace pelton
+
+#endif  // PELTON_SQL_CONNECTIONS_ROCKSDB_UTIL_H__
+
