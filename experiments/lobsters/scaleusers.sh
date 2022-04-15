1b7b9b4bdc8f91b57f4197fd2f49be388888cf96
pelton@gcloud <pelton>
2022-04-15 00:28:22 +0000

    Fix scripts

--- experiments/lobsters/scaleusers.sh
+++ experiments/lobsters/scaleusers.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-scales=("0.173", "0.863", "1.73", "2.59", "3.45",  "4.312")
-users=("1000", "5000", "10000", "15000", "20000", "25000")
-loads=("300", "725", "1000", "1000", "1000")
+scales=("0.173" "0.863" "1.73" "2.59" "3.45"  "4.312")
+users=("1000" "5000" "10000" "15000" "20000" "25000")
+loads=("300" "725" "1000" "1000" "1000")
 
 echo "Starting with pelton ..."
 for index in "${!scales[@]}";
 do
   scale=${scales[$index]}
-  users=${users[$index]}
+  user=${users[$index]}
   load=${loads[$index]}
   echo "pelton-${users}"
   bazel run -c opt //:lobsters-harness -- \
@@ -18,7 +18,7 @@ do
     --backend pelton \
     --prime \
     --scale_everything \
-    "mysql://pelton:password@10.128.0.18:10001/lobsters" > pelton-${users}.txt 2>&1
+    "mysql://pelton:password@10.128.0.18:10001/lobsters" > pelton-${user}.txt 2>&1
   mariadb -P10001 --host=10.128.0.18 -e "STOP";
   sleep 3
 done
@@ -28,7 +28,7 @@ sleep 5
 for index in "${!scales[@]}";
 do
   scale=${scales[$index]}
-  users=${users[$index]}
+  user=${users[$index]}
   load=${loads[$index]}
   echo "baseline-${users}"
   bazel run -c opt //:lobsters-harness -- \
@@ -39,6 +39,6 @@ do
     --backend rocks-mariadb \
     --prime \
     --scale_everything \
-    "mysql://pelton:password@10.128.0.18:3306/lobsters" > base-${users}.txt 2>&1
+    "mysql://pelton:password@10.128.0.18:3306/lobsters" > base-${user}.txt 2>&1
   sleep 3
 done
--- experiments/lobsters/server-scaleusers.sh
+++ experiments/lobsters/server-scaleusers.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 cd /home/pelton/pelton
 
-scales=("0.173", "0.863", "1.73", "2.59", "3.45",  "4.312")
-users=("1000", "5000", "10000", "15000", "20000", "25000")
+scales=("0.173" "0.863" "1.73" "2.59" "3.45"  "4.312")
+users=("1000" "5000" "10000" "15000" "20000" "25000")
 
 echo "pelton ...."
 sudo service mariadb stop
 for index in "${!scales[@]}";
 do
   scale=${scales[$index]}
-  users=${users[$index]}
-  echo "pelton-$user"
-  ./run.sh opt --hostname="10.128.0.18:10001" > log-${users}.txt
+  user=${users[$index]}
+  echo "pelton-$users"
+  ./run.sh opt --hostname="10.128.0.18:10001" > log-${user}.txt
 done
 
 echo "baseline ..."
