8879b62ceebd4f181b87418ef408f3b0556c637b
pelton <pelton>
2022-04-20 14:49:03 +0000

    small fix to ssd setup script

--- scripts/gcloud-nsdi-ssd-setup.sh
+++ scripts/gcloud-nsdi-ssd-setup.sh
@@ -6,8 +6,8 @@ mkdir -p /mnt/disks/my-ssd
 mount /dev/nvme0n1 /mnt/disks/my-ssd
 chown mysql.mysql /mnt/disks/my-ssd
 chmod 777 /mnt/disks/my-ssd
-sudo cp /var/lib/mysql /mnt/disks/my-ssd/ -r
-chown mysql.mysql /mnt/disks/my-ssd/mysql
+cp /var/lib/mysql /mnt/disks/my-ssd/ -r
+chown mysql.mysql -R /mnt/disks/my-ssd/mysql
 
 
 # Configure mariadb to store database in directory
