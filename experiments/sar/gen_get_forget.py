dffb72adbde14a4701c374a3824ae61d4ac9dbdd
Kinan Dak Albab <Kinan.bab.14@gmail.com>
2022-04-20 20:14:16 -0400

    improve sar

--- experiments/sar/gen_get_forget.py
+++ experiments/sar/gen_get_forget.py
@@ -2,18 +2,18 @@ import sys
 import random
 
 total_number = 5000
-user_count = 1000
+user_count = int(sys.argv[1])
 users = set()
 for i in range(user_count):
   user_id = None
-  if sys.argv[2] == 'random':
+  if sys.argv[3] == 'random':
     user_id = random.randint(user_count + 1, total_number)
     while user_id in users:
       user_id = random.randint(user_count + 1, total_number)    
   else:
     user_id = i
     
-  if sys.argv[1] == 'get':
+  if sys.argv[2] == 'get':
     print('GDPR GET users {};'.format(user_id))  
   else:
     print('GDPR FORGET users {};'.format(user_id))
