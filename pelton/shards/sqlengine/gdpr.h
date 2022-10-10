445e101b498e5358a9438b5a53714b68eee0fa92
Artem Agvanian <artemagvanian@icloud.com>
2022-10-10 16:21:28 +0000

    add parts of GDPR Forget working with new metadata

---
+++ pelton/shards/sqlengine/gdpr.h
@@ -0,0 +1,27 @@
+// GDPR statements handling (FORGET and GET).
+
+#ifndef PELTON_SHARDS_SQLENGINE_GDPR_H_
+#define PELTON_SHARDS_SQLENGINE_GDPR_H_
+
+#include "absl/status/statusor.h"
+#include "pelton/connection.h"
+#include "pelton/dataflow/state.h"
+#include "pelton/shards/state.h"
+#include "pelton/shards/types.h"
+#include "pelton/sql/result.h"
+#include "pelton/sqlast/ast.h"
+
+namespace pelton {
+namespace shards {
+namespace sqlengine {
+namespace gdpr {
+
+absl::StatusOr<sql::SqlResult> Shard(const sqlast::GDPRStatement &stmt,
+                                     Connection *connection);
+
+}  // namespace gdpr
+}  // namespace sqlengine
+}  // namespace shards
+}  // namespace pelton
+
+#endif  // PELTON_SHARDS_SQLENGINE_GDPR_H_
