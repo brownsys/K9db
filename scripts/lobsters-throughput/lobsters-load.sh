d1b75c9a6f10cb72ac31ac121bf3b8a4ee60cb9a
Kinan Dak Albab <kinan.bab.14@gmail.com>
2023-03-07 20:37:04 -0500

    Throughput script improved

--- scripts/lobsters-throughput/lobsters-load.sh
+++ scripts/lobsters-throughput/lobsters-load.sh
@@ -26,3 +26,19 @@ do
   # Sleep a while.
   sleep 20
 done
+
+# Same but for baseline!
+for RS in "1000" "1250" "1500" "1750" "2000" "2500" "3000" "4000" "5000" "7500"
+do
+  echo "Datascale $DS"
+  bazel run -c opt //:lobsters-harness -- \
+    --runtime $RT --datascale $DS --reqscale $RS --queries pelton \
+    --backend rocks-mariadb --prime --scale_everything \
+    "mysql://root:password@$TARGET_IP:10001/lobsters" \
+    > "$OUT/baseline$RS.out" 2>&1
+
+  # Sleep a while.
+  sleep 20
+done
+
+mariadb -P10001 --host=$TARGET_IP -e "STOP";
--- scripts/lobsters-throughput/lobsters-server.sh
+++ scripts/lobsters-throughput/lobsters-server.sh
@@ -24,3 +24,13 @@ echo "Terminated"
 echo "Terminated"
 ./run.sh opt --hostname="$LOCAL_IP:10001"
 echo "Terminated"
+
+# Start mariadb.
+sudo service mariadb start
+
+# Wait for close signal
+./run.sh opt --hostname="$LOCAL_IP:10001"
+echo "Baseline Done!"
+
+# Stop mariadb.
+sudo service mariadb stop
