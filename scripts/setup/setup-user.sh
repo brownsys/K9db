ac58ba0fb6fd6e5bd4103a4981b65dbf0274e85d
Kinan Dak Albab <kinan.bab.14@gmail.com>
2023-04-14 19:20:43 -0400

    Cargo raze has a broken dependecy, fix

--- scripts/setup/setup-user.sh
+++ scripts/setup/setup-user.sh
@@ -8,9 +8,10 @@ sudo chown $USER.$USER -R /tmp/pelton
 # install rust
 curl https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
-cargo install cargo-raze
+cargo install --version 0.15.0 cargo-raze
 
 # Run cargo raze.
+mkdir -p /tmp/cargo-raze/doesnt/exist/
 cd pelton/proxy && cargo raze && cd -
 cd experiments/lobsters && cargo raze && cd -
 cd experiments/ownCloud && cargo raze && cd -
