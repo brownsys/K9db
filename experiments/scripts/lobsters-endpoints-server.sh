93080db8bc2ae3c125f31e03be914540b414efd0
Kinan Dak Albab <kinan.bab.14@gmail.com>
2023-05-04 15:31:22 -0400

    Scripts use either LOCAL_IP or 127.0.0.1

--- experiments/scripts/lobsters-endpoints-server.sh
+++ experiments/scripts/lobsters-endpoints-server.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 mkdir -p /mnt/disks/my-ssd/k9db
 
+DB_IP=$LOCAL_IP
+if [[ $LOCAL_IP == "" ]]; then
+  DB_IP="127.0.0.1"
+fi
+
 echo "Check load instance for output"
+echo "Running server on $DB_IP:10001"
 
 # Restart mariadb.
 # Load script will now run the baseline load.
 echo "MariaDB Baseline"
 sudo service mariadb restart
-./run.sh opt --hostname="$LOCAL_IP:10001"  # wait for kill signal from load
+./run.sh opt --hostname="$DB_IP:10001"  # wait for kill signal from load
 
 # Baseline load done.
 sudo service mariadb stop
@@ -15,12 +21,12 @@ sudo service mariadb stop
 # Run encrypted K9db.
 echo "K9db (encrypted)"
 rm -rf /mnt/disks/my-ssd/k9db/k9db
-bazel run //:k9db --config=opt -- --hostname="$LOCAL_IP:10001" --db_path="/mnt/disks/my-ssd/k9db/"
+bazel run //:k9db --config=opt -- --hostname="$DB_IP:10001" --db_path="/mnt/disks/my-ssd/k9db/"
 
 # Run unencrypted K9db.
 echo "K9db (unencrypted)"
 rm -rf /mnt/disks/my-ssd/k9db/k9db
-bazel run //:k9db --config=opt --encryption=off -- --hostname="$LOCAL_IP:10001" --db_path="/mnt/disks/my-ssd/k9db/"
+bazel run //:k9db --config=opt --encryption=off -- --hostname="$DB_IP:10001" --db_path="/mnt/disks/my-ssd/k9db/"
 
 # Done.
 echo "Experiment done"
--- experiments/scripts/lobsters-scale-server.sh
+++ experiments/scripts/lobsters-scale-server.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 mkdir -p /mnt/disks/my-ssd/k9db
 
+DB_IP=$LOCAL_IP
+if [[ $LOCAL_IP == "" ]]; then
+  DB_IP="127.0.0.1"
+fi
+
 echo "Check load instance for output"
+echo "Running server on $DB_IP:10001"
 
 # Stop mariadb.
 sudo service mariadb stop
@@ -10,7 +16,7 @@ for DS in "1.72" "3.44" "4.31" "5.17" "6.89" "7.76" "8.62" "10.34" "12.9" "17.24
 do
   echo "Datascale $DS"
   rm -rf /mnt/disks/my-ssd/k9db/k9db
-  bazel run //:k9db --config=opt -- --hostname="$LOCAL_IP:10001" --db_path="/mnt/disks/my-ssd/k9db/"
+  bazel run //:k9db --config=opt -- --hostname="$DB_IP:10001" --db_path="/mnt/disks/my-ssd/k9db/"
 done
 
 
